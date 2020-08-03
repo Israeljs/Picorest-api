3dd1105956266a773c6878b12600ae353f9e0a31
israel <israel.silva@a.ficr.edu.br>
2020-08-03 01:25:27 -0300

    base

---
+++ API/src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_ROOT,
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  define: {
+    freezeTableName: true,
+    underscored: false,
+  },
+}
